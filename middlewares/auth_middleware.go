a4b5169100fda33677fb75faad2f0c53a37dcd48
HRitsFadhila <m.harits.fadhila@gmail.com>
2026-06-28 15:09:26 +0700

    make Helper for hashing password, generate jwt and message validator

---
+++ middlewares/auth_middleware.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/HRitsFadhila/golang-backend/config"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte(config.GetEnv("JWT_SECRET", "secret_key"))
+
+func AuthMiddleware() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		if tokenString == ""{
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error" : "Token is required",
+			})
+			c.Abort()
+			return 
+		}
+
+		tokenString = strings.TrimPrefix(tokenString, "Bearer")
+
+		claims := &jwt.RegisteredClaims{}
+
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid{
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error" : "Invalid token",
+			})
+			c.Abort()
+			return 
+		}
+
+		c.Set("username", claims.Subject)
+
+		c.Next()
+
+	}
+
+}
